9eb6c0cbf6eeb39dadf4734477f940c754764cef
Tivotal <121335284+Tivotal@users.noreply.github.com>
2023-11-22 07:14:42 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Created by Tivotal */
+
+let btn = document.querySelector(".btn");
+
+btn.onmousemove = function (e) {
+  let x = e.pageX - btn.offsetLeft;
+  let y = e.pageY - btn.offsetTop;
+
+  btn.style.setProperty("--x", x + "px");
+  btn.style.setProperty("--y", y + "px");
+};
